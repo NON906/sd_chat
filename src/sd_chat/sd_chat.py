4dcedd64d206fa798feaccea24eb09aa45c3081a
NON906 <mumeigames@gmail.com>
2024-12-13 13:36:26 +0900

    ビルド設定

--- src/sd_chat/sd_chat.py
+++ src/sd_chat/sd_chat.py
@@ -1,7 +1,7 @@
 from fastmcp import FastMCP, Image
 
-from src.sdapi_webui import SDAPI_WebUI
-from src.settings import CheckPointSettings
+from .sdapi_webui import SDAPI_WebUI
+from .settings import CheckPointSettings
 
 mcp = FastMCP("Stable Diffusion MCP Server")
 
@@ -23,6 +23,9 @@ Return value:
     checkpoint_settings = CheckPointSettings(name='animagineXLV31_v31')
     ret_base = await sd_api.txt2img(prompt, checkpoint_settings)
     return Image(data=ret_base, format="png")
+
+def main():
+    mcp.run()
 
 if __name__ == "__main__":
     mcp.run()
